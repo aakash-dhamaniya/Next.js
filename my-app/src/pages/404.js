74d0e2221abd1f63ba3d3c2c052d4a5d757ee6a2
aakash-dhamaniya <aakashdhamaniya.dev@gmail.com>
2023-03-24 03:55:59 +0530

    learned how to get back automaticly to home page after wrong 404 error within 3 second

--- my-app/src/pages/404.js
+++ my-app/src/pages/404.js
@@ -1,7 +1,13 @@
-import React from "react";
+import { useEffect } from "react";
 import { useRouter } from "next/router";
 const ErrorPage = () => {
   const router = useRouter();
+  useEffect(() => {
+    setTimeout(() => {
+      router.push("/");
+    }, 3000);
+  }, []);
+
   return (
     <>
       <div>
