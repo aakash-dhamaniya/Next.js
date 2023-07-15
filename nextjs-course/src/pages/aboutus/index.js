bfc9820eca6d68d52bc7c045d486027790ac3518
aakash-dhamaniya <aakashdhamaniya.dev@gmail.com>
2023-07-16 04:43:21 +0530

    use Link tag and make name clikable so that it can show its role

--- nextjs-course/src/pages/aboutus/index.js
+++ nextjs-course/src/pages/aboutus/index.js
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export const details = [
   { id: "1", name: "Yash", role: "Senior Developer" },
 
@@ -6,7 +8,16 @@ export const details = [
   { id: "3", name: "Suresh", role: "Frontend Developer" },
 ];
 function about() {
-  return <div>about</div>;
+  const devloperName = details.map((item) => {
+    return (
+      <div key={item.id}>
+        <Link href={`aboutus/${item.id}`}>
+          <h1>{item.name}</h1>
+        </Link>
+      </div>
+    );
+  });
+  return <div>{devloperName}</div>;
 }
 
 export default about;
